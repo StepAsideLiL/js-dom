6d20a4a0278538ee37ad47d0278fb096d92d3663
stepasidelil <rifat6633@gmail.com>
2022-02-11 11:53:19 +0600

    project 1: change background color randomly

---
+++ src/project001/app.js
@@ -0,0 +1,21 @@
+/**
+ * Project requirement
+ * - Change the background color by generating random color
+ */
+
+// Select the elements.
+const body = document.getElementsByTagName('body')[0];
+const btn = document.querySelector('.change-btn');
+
+// add event listener to the button element.
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    body.style.backgroundColor = changeBackgroundColor();
+});
+
+// function to change background color randomly.
+function changeBackgroundColor() {
+    var backgroungColor = Math.floor(Math.random()*16777215).toString(16);
+    console.log('#'+backgroungColor);
+    return '#'+backgroungColor;
+}
