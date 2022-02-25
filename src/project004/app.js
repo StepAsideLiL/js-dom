3381872ead24da8a005e84beab8cbbc3317aa140
stepasidelil <rifat6633@gmail.com>
2022-02-26 02:19:12 +0600

    project 4: random color with toast message when copied

---
+++ src/project004/app.js
@@ -0,0 +1,77 @@
+/**
+ * Project requirement
+ * - Ramdomly change background color by clicking a button.
+ * - Show the hax code in input field.
+ * - Also a button to copy the hax code in clipboard. 
+ * - Show a toast message when copied the color.
+ */
+
+const body = document.getElementsByTagName( 'body' )[0];
+const input = document.querySelector( '.input-box' );
+const copyBtn = document.querySelector( '.input-copy' ); 
+const btn = document.querySelector( '.change-btn' );
+var haxColorCode;
+
+btn.addEventListener( 'click', function( e ) {
+    e.preventDefault();
+    haxColorCode = generateHaxBackgroundColor();
+    body.style.backgroundColor = haxColorCode;
+    input.value = haxColorCode;
+} );
+
+copyBtn.addEventListener( 'click', function( e ) {
+    e.preventDefault();
+    navigator.clipboard.writeText( input.value );
+    generateToastMessage( `${haxColorCode} Copied!` );
+} );
+
+/**
+ * Generate random color hax code.
+ * (16777215)10 = (ffffff)16
+ * 
+ * @return string color hax code
+ */
+function generateHaxBackgroundColor() {
+    var randomHexColor = Math.floor( Math.random() * 16777215 ).toString(16);
+    console.log( `#${randomHexColor}` );
+    return `#${randomHexColor}`;
+}
+
+/**
+ * Generate toast message.
+ * 
+ * @param 
+ * 
+ * @return void
+ */
+function generateToastMessage( message ) {
+    const div = document.createElement( 'div' );
+
+    div.innerText = message;
+    div.className = 'toast-message toast-message-slide-in';
+
+    div.addEventListener( 'click', function(e) {
+        e.preventDefault();
+        div.classList.remove( 'toast-message-slide-in' );
+        div.classList.add( 'toast-message-slide-out' );
+
+        div.addEventListener( 'animationend', function(e) {
+            e.preventDefault();
+            div.remove();
+        } );
+    } );
+
+    if ( div ) {
+        setTimeout( function(e) {
+            div.classList.remove( 'toast-message-slide-in' );
+            div.classList.add( 'toast-message-slide-out' );
+    
+            div.addEventListener( 'animationend', function(e) {
+                e.preventDefault();
+                div.remove();
+            } );
+        }, 3000 );
+    }
+    
+    document.body.appendChild( div );
+}
