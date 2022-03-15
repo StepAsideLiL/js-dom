8fbc8240a9d57ae240865aea46d933baaf14f765
stepasidelil <rifat6633@gmail.com>
2022-03-15 10:04:11 +0600

    project 2

--- src/project002/app.js
+++ src/project002/app.js
@@ -4,18 +4,25 @@
  * - Show the hex code in input field.
  */
 
+// Select elements
 const body = document.getElementsByTagName( 'body' )[0];
 const input = document.querySelector( '.input-box' );
 const btn = document.querySelector( '.change-btn' );
 
+// Add click event listener to button
 btn.addEventListener( 'click', function(e) {
     e.preventDefault();
-    var hexColorCode = generatehexBackgroundColor();
+    var hexColorCode = generateHexBackgroundColor();
     body.style.backgroundColor = hexColorCode;
     input.value = hexColorCode;
 } );
 
-function generatehexBackgroundColor() {
+/**
+ * Generate random hex color code.
+ * 
+ * @returns {string} hex color code.
+ */
+function generateHexBackgroundColor() {
     var randomHexColor = Math.floor( Math.random() * 16777215 ).toString(16);
     console.log( `#${randomHexColor}` );
     return `#${randomHexColor}`;
